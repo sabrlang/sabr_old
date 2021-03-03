75871c64f9dce34797ca7610823505c90472bb65
mhComa <bmh4080@naver.com>
2021-03-03 16:19:28 +0900

    linux

--- src/interpreter/interpreter.c
+++ src/interpreter/interpreter.c
@@ -719,20 +719,21 @@ bool interpreter_run(interpreter* inter) {
 					if (!vector_push_back(value, &value_reverser, v)) goto FAILURE_STDIN;
 				}
 			#else
-				char* line;
+				char* line = NULL;
 				size_t len, rc;
 				ssize_t read;
 				char32_t out;
 
 				mbstate_t state;
 				
-				while ((read = getline(&line, &len, stdin)) == -1) {
+				if ((read = getline(&line, &len, stdin)) == -1) {
 					while (rc = mbrtoc32(&out, line, len, &state)) {
 						if ((rc > ((size_t) -4)) || (rc == 0)) goto FAILURE_STDIN;
 						if (out == 10) break;
 						len--;
 						read++;
 						v.u = out;
+						printf("%lu ", v.u);
 						if (!vector_push_back(value, &value_reverser, v)) goto FAILURE_STDIN;
 					}
 				}
